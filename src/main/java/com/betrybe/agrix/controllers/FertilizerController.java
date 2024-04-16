6cea0271f59fbe1c40935fd923da91bcdc7ff3b5
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-16 15:51:00 -0300

    feat: create FertilizerController and insertFertilizer method

---
+++ src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
@@ -0,0 +1,29 @@
+package com.betrybe.agrix.controllers;
+
+import com.betrybe.agrix.models.entities.Fertilizer;
+import com.betrybe.agrix.services.FertilizerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fertilizers")
+public class FertilizerController {
+
+  private final FertilizerService fertilizerService;
+
+  @Autowired
+  public FertilizerController(FertilizerService fertilizerService) {
+    this.fertilizerService = fertilizerService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Fertilizer> insertFertilizer(@RequestBody FertilizerDto fertilizerDto) {
+    Fertilizer newFertilizer = fertilizerService.insertFertilizer();
+    return ResponseEntity.status(HttpStatus.CREATED).body(newFertilizer);
+  }
+}
