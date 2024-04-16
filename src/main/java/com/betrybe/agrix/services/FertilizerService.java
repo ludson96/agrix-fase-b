840b84783326267e598a083b13854cae94e32fed
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-16 15:47:13 -0300

    feat: create FertilizerService and insertFertilizer method

---
+++ src/main/java/com/betrybe/agrix/services/FertilizerService.java
@@ -0,0 +1,21 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Fertilizer;
+import com.betrybe.agrix.models.repositories.FertilizerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FertilizerService {
+
+  private final FertilizerRepository fertilizerRepository;
+
+  @Autowired
+  public FertilizerService(FertilizerRepository fertilizerRepository) {
+    this.fertilizerRepository = fertilizerRepository;
+  }
+
+  public Fertilizer insertFertilizer(Fertilizer fertilizer) {
+    return  fertilizerRepository.save(fertilizer);
+  }
+}
