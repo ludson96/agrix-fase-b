465582129e3194106e9d1a80bb32486472a136d7
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-17 15:36:16 -0300

    feat: create test for PersonService

--- src/test/java/com/betrybe/agrix/solution/PersonServiceTest.java
+++ src/test/java/com/betrybe/agrix/solution/PersonServiceTest.java
@@ -1,90 +1,95 @@
-//package com.betrybe.agrix.solution;
-//
-//import static  org.junit.jupiter.api.Assertions.*;
-//
-//import com.betrybe.agrix.ebytr.staff.entity.Person;
-//import com.betrybe.agrix.ebytr.staff.exception.PersonNotFoundException;
-//import com.betrybe.agrix.ebytr.staff.security.Role;
-//import com.betrybe.agrix.ebytr.staff.service.PersonService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//@DisplayName("Teste Person da camada de logica BD H2.")
-//public class PersonServiceTest {
-//
-//  @Autowired
-//  PersonService personService;
-//
-//  @Test
-//  @DisplayName("Deveria retornar um Person pelo id")
-//  public void getPersonByIdTest() {
-//    Person personToCreate = new Person();
-//    personToCreate.setUsername("test user 1");
-//    personToCreate.setPassword("test password");
-//    personToCreate.setRole(Role.USER);
-//
-//    Person savedPerson = personService.create(personToCreate);
-//    Long savedPersonId = savedPerson.getId();
-//
-//    Person returnedPerson = personService.getPersonById(savedPersonId);
-//
-//    assertEquals(returnedPerson.getId(), savedPersonId);
-//    assertEquals(returnedPerson.getUsername(), personToCreate.getUsername());
-//    assertEquals(returnedPerson.getPassword(), personToCreate.getPassword());
-//    assertEquals(returnedPerson.getRole(), personToCreate.getRole());
-//  }
-//
-//  @Test
-//  @DisplayName("Deveria lançar uma exceção caso não encontre um id fornecido")
-//  public void getPersonByIdThrowsTest() {
-//    assertThrows(PersonNotFoundException.class, () -> personService.getPersonById(123L));
-//  }
-//
-//  @Test
-//  @DisplayName("Deveria retornar um Person pelo Username")
-//  public void getPersonByUsernameTest() {
-//    Person personToCreate = new Person();
-//    personToCreate.setUsername("test user 2");
-//    personToCreate.setPassword("test password");
-//    personToCreate.setRole(Role.USER);
-//
-//    Long savedPersonId = personService.create(personToCreate).getId();
-//
-//    Person personByUsername = personService.getPersonByUsername(personToCreate.getUsername());
-//
-//    assertEquals(personByUsername.getId(), savedPersonId);
-//    assertEquals(personByUsername.getUsername(), personToCreate.getUsername());
-//    assertEquals(personByUsername.getPassword(), personToCreate.getPassword());
-//    assertEquals(personByUsername.getRole(), personToCreate.getRole());
-//  }
-//
-//  @Test
-//  @DisplayName("Deveria lançar uma exceção caso não encontre um nome fornecido")
-//  public void getPersonByUsernameThrowsTest() {
-//    assertThrows(PersonNotFoundException.class,
-//        () -> personService.getPersonByUsername("anything"));
-//  }
-//
-//  @Test
-//  @DisplayName("Deveria criar um person corretamente")
-//  public void createPersonTest() {
-//    Person personToCreate = new Person();
-//    personToCreate.setUsername("test user 3");
-//    personToCreate.setPassword("test password");
-//    personToCreate.setRole(Role.USER);
-//
-//    Long savedPersonId = personService.create(personToCreate).getId();
-//
-//    Person returnedPerson = personService.getPersonById(savedPersonId);
-//
-//    assertEquals(returnedPerson.getId(), savedPersonId);
-//    assertEquals(returnedPerson.getUsername(), personToCreate.getUsername());
-//    assertEquals(returnedPerson.getPassword(), personToCreate.getPassword());
-//    assertEquals(returnedPerson.getRole(), personToCreate.getRole());
-//  }
-//}
+package com.betrybe.agrix.solution;
+
+import static  org.junit.jupiter.api.Assertions.*;
+
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import com.betrybe.agrix.ebytr.staff.exception.PersonNotFoundException;
+import com.betrybe.agrix.ebytr.staff.security.Role;
+import com.betrybe.agrix.ebytr.staff.service.PersonService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@DisplayName("Teste Person da camada de logica usando BD H2.")
+public class PersonServiceTest {
+
+  @Autowired
+  PersonService personService;
+
+  @Test
+  @Transactional
+  @DirtiesContext
+  @DisplayName("Deveria criar um person corretamente")
+  public void createPersonTest() {
+    Person person = new Person();
+    person.setUsername("Ludson");
+    person.setPassword("123456");
+    person.setRole(Role.USER);
+
+    Person savedPerson = personService.create(person);
+
+    assertNotNull(savedPerson.getId());
+    assertEquals(person.getUsername(), savedPerson.getUsername());
+    assertEquals(person.getPassword(), savedPerson.getPassword());
+    assertEquals(person.getRole(), savedPerson.getRole());
+  }
+
+  @Test
+  @DisplayName("Deveria retornar um Person pelo Username")
+  @Transactional
+  @DirtiesContext
+  public void getPersonByUsernameTest() {
+    Person person = new Person();
+    person.setUsername("Ludson");
+    person.setPassword("123456");
+    person.setRole(Role.USER);
+
+    Person savedPerson = personService.create(person);
+
+    Person personByUsername = personService.getPersonByUsername(savedPerson.getUsername());
+
+    assertEquals(savedPerson.getId(), personByUsername.getId());
+    assertEquals(savedPerson.getUsername(), personByUsername.getUsername());
+    assertEquals(savedPerson.getPassword(), personByUsername.getPassword());
+    assertEquals(savedPerson.getRole(), personByUsername.getRole());
+  }
+
+  @Test
+  @DisplayName("Deveria retornar um Person pelo id")
+  @Transactional
+  @DirtiesContext
+  public void getPersonByIdTest() {
+    Person person = new Person();
+    person.setUsername("Ludson");
+    person.setPassword("123456");
+    person.setRole(Role.USER);
+
+    Person savedPerson = personService.create(person);
+
+    Person personById = personService.getPersonById(savedPerson.getId());
+
+    assertEquals(savedPerson.getId(), personById.getId());
+    assertEquals(savedPerson.getUsername(), personById.getUsername());
+    assertEquals(savedPerson.getPassword(), personById.getPassword());
+    assertEquals(savedPerson.getRole(), personById.getRole());
+  }
+
+  @Test
+  @DisplayName("Deveria lançar uma exceção caso não encontre um id fornecido")
+  public void getPersonByIdThrowsTest() {
+    assertThrows(PersonNotFoundException.class, () -> personService.getPersonById(123L));
+  }
+
+  @Test
+  @DisplayName("Deveria lançar uma exceção caso não encontre um nome fornecido")
+  public void getPersonByUsernameThrowsTest() {
+    assertThrows(PersonNotFoundException.class,
+        () -> personService.getPersonByUsername("não fornecido"));
+  }
+}
